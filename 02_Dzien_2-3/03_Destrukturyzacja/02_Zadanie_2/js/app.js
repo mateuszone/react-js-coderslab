52432ffa38dd7d0225781f446229a35382728289
Mateusz Czaja <Mathewczaja@gmail.com>
2021-07-03 21:03:23 +0200

    dzien 2/3 zadanie 3.2

---
+++ 02_Dzien_2-3/03_Destrukturyzacja/02_Zadanie_2/js/app.js
@@ -0,0 +1,9 @@
+const slider = {
+  type: "infinite",
+  numberOfItems: 10,
+  center: true,
+  autoStart: true
+}
+
+let {type, autoStart} = slider;
+console.log(type, autoStart);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const entryPath = "02_Dzien_2-3/03_Destrukturyzacja/01_Zadanie_1";
+const entryPath = "02_Dzien_2-3/03_Destrukturyzacja/02_Zadanie_2";
 
 module.exports = {
   entry: `./${entryPath}/js/app.js`,
