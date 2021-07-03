759bec085f078c820dae1ed182dd9dfc3e4dd682
Mateusz Czaja <Mathewczaja@gmail.com>
2021-07-03 20:42:26 +0200

    dzien2/3 zadanie 2

---
+++ 02_Dzien_2-3/01_Operatory_rest_i_spread/02_Zadanie_2/js/app.js
@@ -0,0 +1,5 @@
+const fruits = ['apple', 'orange', 'mango', 'banana' ]
+const vegetables = ['broccoli', 'cauliflower', 'onion']
+
+const mix = [...fruits,...vegetables]
+console.log(mix);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const entryPath = "02_Dzien_2-3/01_Operatory_rest_i_spread/01_Zadanie_1";
+const entryPath = "02_Dzien_2-3/01_Operatory_rest_i_spread/02_Zadanie_2";
 
 module.exports = {
   entry: `./${entryPath}/js/app.js`,
